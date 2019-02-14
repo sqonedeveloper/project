ac887138b5714aec08ad07287369b21c12cba644
sqonedeveloper <sqone.developer@ar-raniry.ac.id>
2019-02-14 22:09:49 +0700

    commit

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+const buildPath = path.resolve(__dirname, 'assets/cetak/bundle');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(buildPath),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "babel-loader"
+			}
+		}, {
+			test: /\.html$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "html-loader"
+			}
+		}]
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				extractComments: true,
+				parallel: true
+			})
+		],
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+					name: 'vendor',
+					chunks: 'all'
+				}
+			}
+		}
+	},
+	plugins: [
+		new CleanWebpackPlugin([buildPath]),
+		new HtmlWebPackPlugin({
+			template: path.resolve(__dirname, './src/index.html'),
+			excludeChunks: ['base'],
+			minify: {
+				collapseWhitespace: true,
+				collapseInlineTagWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true
+			}
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
+			}
+		}),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new ScriptExtHtmlWebpackPlugin({
+			defaultAttribute: 'defer'
+		})
+	],
+	performance: {
+		hints: false
+	},
+	devServer: {
+		quiet: true,
+		host: 'localhost',
+		watchContentBase: true,
+		compress: true,
+		port: 8080
+	},
+	devtool: 'cheap-eval-source-map'
+}
