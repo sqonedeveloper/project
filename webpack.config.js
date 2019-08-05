4f39489d4930961627a46a20229caee1f5b50d7a
Safrizal <47549239+sqonedeveloper@users.noreply.github.com>
2019-08-05 19:26:44 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const WebpackBar = require('webpackbar');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 const buildPath = path.resolve(__dirname, 'assets/js/admin/bundle');
 const entry_public = {
@@ -78,6 +79,25 @@ module.exports = {
    },
    plugins: [
       new WebpackBar(),
+      new HardSourceWebpackPlugin({
+			cacheDirectory: 'node_modules/.cache/hard-source/[confighash]',
+			configHash: function(webpackConfig) {
+				return require('node-object-hash')({sort: false}).hash(webpackConfig);
+			},
+			environmentHash: {
+				root: process.cwd(),
+				directories: [],
+				files: ['package-lock.json', 'yarn.lock']
+			},
+			info: {
+				mode: 'none',
+				level: 'debug'
+			},
+			cachePrune: {
+				maxAge: 2 * 24 * 60 * 60 * 1000,
+				sizeThreshold: 50 * 1024 * 1024
+			}
+		}),
       new webpack.optimize.ModuleConcatenationPlugin(),
       new HtmlWebPackPlugin({
          template: path.resolve(__dirname, './src/index.html'),
